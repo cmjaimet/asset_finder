61de7527e031495f03a432d260559c55a882b62c
Charles Jaimet <charles@calmseamedia.com>
2018-08-19 19:40:51 -0400

    Document and tidy a bit

--- classes/DisplayWeb.php
+++ classes/DisplayWeb.php
@@ -2,13 +2,9 @@
 namespace AssetFinder;
 /**
 * Next steps:
-* 	- get handles and actions from wp_option as JSON
-* 	- handle in 	add_action( 'wp_head', 'function name', n, 0 ); // use n = 2: where n > 1 which is when enqueuing happens and below 7-9 where style,script printing happens
+* 	- add changes to options
+* 	- read changes from options
 * 	- parse JSON and remove/late-load styles and scripts as requested
-* 	- Remove Style with wp_dequeue_style( $handle ), wp_deregister_style( $handle );
-* 	- Late-load Style by setting media="lateload" then JS triggers in footer to set it back to original value ("all" if origin == "")
-* 	- Remove Script with wp_dequeue_script( $handle );
-* 	- Late-load Script by setting .extra['group'] = 1
 */
 
 $asset_finder = new \AssetFinder\DisplayWeb();
@@ -25,16 +21,19 @@ class DisplayWeb {
 		add_action( 'wp_print_styles', array( $this, 'modify_asset_loading' ), 100, 0 );
 		add_action( 'wp_footer', array( $this, 'lateload_styles' ), 10, 0 );
 		wp_enqueue_script( 'asset_finder_lateload', ASSET_FINDER_URI . 'js/web.js', array(), 'v.1.0.0', false );
+		wp_enqueue_script( 'asset_finder_webtest', ASSET_FINDER_URI . 'js/test.js', array(), 'v.1.0.1', false );
 	}
 
+	/**
+	*
+	*/
 	public function modify_asset_loading() {
 		// get option
 		// iterate through styles and scripts
 		// foreach style { $this->handle_style( $handle, $action ) }
 		// foreach script { $this->handle_script( $handle, $action ) }
-		// $this->remove_style( 'twentyseventeen-style' );
-		// $this->remove_script( 'admin-bar' );
-		$this->handle_style( 'twentyseventeen-style', 1 ); //twentyseventeen/style.css
+		// $this->handle_style( 'twentyseventeen-style', 1 ); //twentyseventeen/style.css
+		// $this->handle_script( 'asset_finder_webtest', 1 );
 	}
 
 	private function handle_style( $handle, $action ) {
@@ -68,6 +67,7 @@ class DisplayWeb {
 	}
 
 	public function lateload_styles() {
+		// cmj: enqueue this inline
 		echo '<script type="text/javascript">' . "\n";
 		foreach ( $this->styles as $handle => $media ) {
 			echo "af_lateload( '" . esc_js( $handle ) . "', '" . esc_js( $media ) . "' )\n";
@@ -81,74 +81,8 @@ class DisplayWeb {
 	}
 
 	private function delay_script( $handle ) {
-
-	}
-
-	public function get_assets_in_page() {
-		$json = '';
-		$assets = array();
-		$assets['scripts'] = $this->get_scripts_in_page();
-		$assets['styles'] = $this->get_styles_in_page();
-		$json = json_encode( $assets );
-		$this->create_web_script( $json );
-	}
-
-	private function create_web_script( $message ) {
-		echo "<script type='text/javascript'>
-		var sendMessage = function ( msg ) {
-			window.parent.postMessage( msg, '*' );
-		};
-		sendMessage( JSON.stringify(" . $message . ") );
-		</script>";
-	}
-
-	private function get_scripts_in_page() {
-		$all = wp_scripts()->registered;
-		$output = array();
-		foreach( $all as $slug => $elem ) {
-			if ( ( '' !== trim( $elem->src ) ) && ( false === strpos( $elem->src, 'wp-admin/' ) ) ) {
-				$footer = 0;
-				if ( isset( $elem->extra['group'] ) && ( 1 === intval( $elem->extra['group'] ) ) ) {
-					$footer = 1;
-				}
-				$output[ $slug ] = array(
-					'handle' => $elem->handle,
-					'src' => $elem->src,
-					'footer' => $footer
-				);
-			}
-		}
-		return $output;
-	}
-
-	private function get_styles_in_page() {
-		$all = wp_styles()->registered;
-		$output = array();
-		foreach( $all as $slug => $elem ) {
-			if ( ( '' !== trim( $elem->src ) ) && ( false === strpos( $elem->src, 'wp-admin/' ) ) ) {
-				$media = '';
-				if ( isset( $elem->args ) ) {
-					$media = trim( $elem->args );
-				}
-				$output[ $slug ] = array(
-					'handle' => $elem->handle,
-					'src' => $elem->src,
-					'media' => $media
-				);
-			}
-		}
-		return $output;
-	}
-
-	/**
-	* Add some styles and scripts to the queue to test
-	*/
-	private function add_test_assets() {
-		if ( true === $this->debug ) {
-			wp_enqueue_style( 'asset_finder_style_test', ASSET_FINDER_URI . 'css/af_test.css', array(), 'v.1.0.0', 'screen' );
-			wp_enqueue_script( 'asset_finder_script_head', ASSET_FINDER_URI . 'js/af_test_head.js', array(), 'v.1.0.1', false );
-			wp_enqueue_script( 'asset_finder_script_foot', ASSET_FINDER_URI . 'js/af_test_foot.js', array(), 'v.1.0.5', true );
-		}
+		global $wp_scripts;
+		$wp_scripts->registered[ $handle ]->extra['group'] = 1;
 	}
 
 }
--- classes/Settings.php
+++ classes/Settings.php
@@ -1,8 +1,8 @@
 <?php
 namespace AssetFinder;
 /**
-* Next steps:
-* 	- save handles and actions to wp_option as JSON
+* Logic: The admin opens a web URL in an iframe and instructs it to gather and pass a list of all scripts and styles enqueued on the page
+* We don't want this happening any time other than when the admin is on the settings page, so pass a timestamp five minutes in the future in the query string and only do it when that exists
 */
 
 $asset_finder = new \AssetFinder\Settings();
@@ -11,20 +11,40 @@ class Settings {
 	private $title = 'Asset Finder';
 	private $debug = true;
 
+	/**
+	* Initialize the object
+	*
+	*/
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
-		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 	}
 
-	function admin_init() {
-		wp_enqueue_style( 'asset_finder_style', ASSET_FINDER_URI . 'css/admin.css', array(), 'v.1.0.0', 'screen' );
-		wp_enqueue_script( 'asset_finder_script', ASSET_FINDER_URI . 'js/admin.js', array(), 'v.1.0.1', true );
+	/**
+	* Enqueue scripts and styles selectively bsed on admin screen
+	*
+	*/
+	function admin_enqueue_scripts() {
+		$screen = get_current_screen();
+		if ( 'settings_page_asset-finder-settings' === $screen->id ) {
+			wp_enqueue_style( 'asset_finder_style', ASSET_FINDER_URI . 'css/admin.css', array(), 'v.1.0.0', 'screen' );
+			wp_enqueue_script( 'asset_finder_script', ASSET_FINDER_URI . 'js/admin.js', array(), 'v.1.0.1', true );
+		}
 	}
 
+	/**
+	* Create the admin menus required by the plugin
+	*
+	*/
 	function admin_menu() {
 		add_options_page( 'Asset Finder', 'Asset Finder', 'manage_options', 'asset-finder-settings', array( $this, 'settings_page' ) );
 	}
 
+	/**
+	* Display the plugin settings page where the enqueue management happens.
+	* This is in settings because it presupposes that oly an administrator should have access.
+	*
+	*/
 	function settings_page() {
 		echo '<h1>' . $this->title . '</h1>';
 		echo '<h2>Scripts</h2>';
@@ -36,8 +56,9 @@ class Settings {
 	}
 
 	/**
-	* Clever way to communicate between iframe and parent
+	* Communicate between iframe and parent
 	* Modifed from Petar Bojinov: https://gist.github.com/pbojinov/8965299
+	*
 	*/
 	private function create_admin_script( $url ) {
 		echo "<script>
@@ -45,10 +66,18 @@ class Settings {
 		</script>";
 	}
 
+	/**
+	* Return a timestamp 5 minutes in the future for admin to communicate request to web safel
+	*
+	*/
 	private function get_settings_timestamp() {
 		return current_time( 'timestamp' ) + ( 5 * 60 ); // now + 5 minutes
 	}
 
+	/**
+	* Return the web URL to be tested for scripts and styles
+	*
+	*/
 	private function get_settings_web_url( $path ) {
 		return site_url() . '/' . $path . '?afts=' . $this->get_settings_timestamp();
 	}
