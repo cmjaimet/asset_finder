dca62093e0661d1902be9f015909735df1863af4
Charles Jaimet <charles@calmseamedia.com>
2018-08-21 10:11:43 -0400

    Plugin header mostly

--- asset-finder.php
+++ asset-finder.php
@@ -2,25 +2,30 @@
 namespace AssetFinder;
 /**
  * Plugin Name: Asset Finder
- * Short Name: asset_finder
+ * Plugin URI:  https://developer.wordpress.org/plugins/asset-finder/
  * Description: Find all JS and CSS assets on a site and allow you to remove or late-load them.
- * Author: Charles Jaimet (cmjaimet@gmail.com)
  * Version: 1.0.0
+ * Author: Charles Jaimet, Sebastien Rolland
+ * Author URI: https://github.com/cmjaimet
+ * Text Domain: asset-finder
+ * Domain Path: /languages
  * Requires at least: 3.0
  * Tested up to: 4.9.8
+ * License: GPLv3
  *
- * This program is free software - you can redistribute it and/or modify
+ * Asset Finder is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Asset Finder is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.	If not, see <http://www.gnu.org/licenses/>.
+ * along with Asset Finder. If not, see
+ * https://www.gnu.org/licenses/gpl.html
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -31,11 +36,14 @@ define( 'ASSET_FINDER_URI', plugins_url( '', __FILE__ ) . '/' );
 define( 'ASSET_FINDER_PATH', plugin_dir_path( __FILE__ ) );
 
 if ( is_admin() ) {
-	require_once( ASSET_FINDER_PATH . 'classes/Settings.php' );
-} else {
-	if ( ! isset( $_GET[ 'afts' ] ) ) {
+	require_once( ASSET_FINDER_PATH . 'classes/AdminSettings.php' );
+} elseif ( ! isset( $_GET[ 'afts' ] ) ) {
 		require_once( ASSET_FINDER_PATH . 'classes/DisplayWeb.php' );
-	} else {
-		require_once( ASSET_FINDER_PATH . 'classes/AssetCollector.php' );
-	}
+} else {
+	require_once( ASSET_FINDER_PATH . 'classes/AssetCollector.php' );
+}
+
+register_uninstall_hook(__FILE__, 'asset_finder_uninstall_plugin');
+function asset_finder_uninstall_plugin() {
+	delete_option( 'asset_finder' );
 }
